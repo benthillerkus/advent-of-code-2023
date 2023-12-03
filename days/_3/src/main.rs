58383db284208554570d7fd3db02cd9f3d3a67dc
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2023-12-03 20:32:38 +0100

    🫥 i yield

--- days/_3/src/main.rs
+++ days/_3/src/main.rs
@@ -1,3 +1,8 @@
+use std::{
+    collections::{BTreeMap, HashMap},
+    sync::OnceLock,
+};
+
 const INPUT: &[u8] = include_bytes!("input.txt");
 
 #[derive(Debug)]
@@ -11,17 +16,94 @@ enum State {
 enum Token {
     Digit,
     Symbol,
+    Gear,
     Junk,
 }
 
 impl From<u8> for Token {
     fn from(value: u8) -> Self {
         match value {
+            b'*' => Self::Gear,
             b'.' | b'\n' => Self::Junk,
             _ if value.is_ascii_digit() => Self::Digit,
             _ => Self::Symbol,
         }
     }
+}
+
+fn vicinity(index: usize) -> impl Iterator<Item = Token> {
+    let index = index as i64;
+    let width = *WIDTH.get().unwrap();
+    let len = INPUT.len() as i64;
+
+    let neighbors = Box::new([
+        index - width - 1,
+        index - width,
+        index - width + 1,
+        index - 1,
+        index,
+        index + 1,
+        index + width - 1,
+        index + width,
+        index + width + 1,
+    ]);
+
+    let neighbors: &'static [i64; 9] = Box::leak(neighbors);
+
+    neighbors
+        .iter()
+        .copied()
+        .filter(move |&index| index >= 0 && index < len)
+        .map(move |index| Token::from(INPUT[index as usize]))
+}
+
+static WIDTH: OnceLock<i64> = OnceLock::new();
+
+fn to_number(s: &[u8]) -> u64 {
+    s.iter()
+        .map(|&b| char::from(b))
+        .collect::<String>()
+        .parse()
+        .unwrap()
+}
+
+fn a() {
+    let mut state = State::Junk;
+    let mut sum = 0u64;
+
+    for (index, token) in INPUT.iter().copied().map(Token::from).enumerate() {
+        state = match (token, state) {
+            // Inside the gutter
+            (Token::Junk | Token::Symbol | Token::Gear, State::Junk) => State::Junk,
+            // Can promote to part?
+            (Token::Digit, State::Junk) => {
+                if vicinity(index).any(|token| token == Token::Symbol || token == Token::Gear) {
+                    State::Part(index)
+                } else {
+                    State::Number(index)
+                }
+            }
+            (Token::Digit, State::Number(start)) => {
+                if vicinity(index).any(|token| token == Token::Symbol || token == Token::Gear) {
+                    State::Part(start)
+                } else {
+                    State::Number(start)
+                }
+            }
+            // Inside a part
+            (Token::Digit, State::Part(start)) => State::Part(start),
+            // A number ends
+            (Token::Junk | Token::Symbol | Token::Gear, State::Number(_)) => State::Junk,
+            // A Part ends
+            (Token::Junk | Token::Symbol | Token::Gear, State::Part(start)) => {
+                let number = to_number(&INPUT[start.to_owned()..index]);
+                sum += number;
+                State::Junk
+            }
+        };
+    }
+
+    println!("{sum}");
 }
 
 fn main() {
@@ -33,79 +115,7 @@ fn main() {
         .unwrap() as i64
         + 1;
 
-    let len = INPUT.len() as i64;
-    let is_symbol_in_vicinity = |index: usize| -> bool {
-        let index = index as i64;
-        let neighbors = [
-            index - width - 1,
-            index - width,
-            index - width + 1,
-            index - 1,
-            index,
-            index + 1,
-            index + width - 1,
-            index + width,
-            index + width + 1,
-        ];
+    WIDTH.set(width).unwrap();
 
-        let mut indexable = neighbors
-            .iter()
-            .copied()
-            .filter(|&index| index >= 0 && index < len);
-
-        // println!(
-        //     "{}",
-        //     indexable
-        //         .clone()
-        //         .map(|index| char::from(INPUT[index as usize]))
-        //         .collect::<String>()
-        // );
-        indexable.any(|index| Token::from(INPUT[index as usize]) == Token::Symbol)
-    };
-
-    let to_number = |s: &[u8]| -> u32 {
-        s.iter()
-            .map(|&b| char::from(b))
-            .collect::<String>()
-            .parse()
-            .unwrap()
-    };
-
-    let mut state = State::Junk;
-    let mut sum = 0u32;
-
-    for (index, token) in INPUT.iter().copied().map(Token::from).enumerate() {
-        state = match (token, state) {
-            // Inside the gutter
-            (Token::Junk | Token::Symbol, State::Junk) => State::Junk,
-            // Can promote to part?
-            (Token::Digit, State::Junk) => {
-                if is_symbol_in_vicinity(index) {
-                    State::Part(index)
-                } else {
-                    State::Number(index)
-                }
-            }
-            (Token::Digit, State::Number(start)) => {
-                if is_symbol_in_vicinity(index) {
-                    State::Part(start)
-                } else {
-                    State::Number(start)
-                }
-            }
-            // Inside a part
-            (Token::Digit, State::Part(start)) => State::Part(start),
-            // A number ends
-            (Token::Junk | Token::Symbol, State::Number(_)) => State::Junk,
-            // A Part ends
-            (Token::Junk | Token::Symbol, State::Part(start)) => {
-                let number = to_number(&INPUT[start.to_owned()..index]);
-                println!("{number}");
-                sum += number;
-                State::Junk
-            }
-        };
-    }
-
-    println!("{sum}")
+    a();
 }
