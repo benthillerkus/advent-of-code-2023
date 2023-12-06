fb0d6b715f8781b7227db107d7d8eb509cc94240
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2023-12-06 09:31:21 +0100

    feat: part 2

--- days/_6/src/main.rs
+++ days/_6/src/main.rs
@@ -1,38 +1,34 @@
-use rayon::prelude::*;
-use std::time::Duration;
+use rayon::{join, prelude::*};
 
 struct Run {
-    duration: Duration,
+    duration: u16,
     record: u16,
 }
 
 impl Run {
-    fn hold(&self, hold: Duration) -> bool {
+    fn hold(&self, hold: u16) -> bool {
         let time_left = self.duration - hold;
-        if time_left.is_zero() {
-            return false;
-        }
-        let velocity = hold.as_millis();
-        let distance = velocity * time_left.as_millis();
-        distance as u16 > self.record
+        let velocity = hold;
+        let distance = velocity * time_left;
+        distance > self.record
     }
 }
 
-const INPUT: &[Run] = &[
+const INPUT: &[Run; 4] = &[
     Run {
-        duration: Duration::from_millis(42),
+        duration: 42,
         record: 308,
     },
     Run {
-        duration: Duration::from_millis(89),
+        duration: 89,
         record: 1170,
     },
     Run {
-        duration: Duration::from_millis(91),
+        duration: 91,
         record: 1291,
     },
     Run {
-        duration: Duration::from_millis(89),
+        duration: 89,
         record: 1467,
     },
 ];
@@ -41,8 +37,8 @@ fn a() {
     let product: usize = INPUT
         .par_iter()
         .map(|run| {
-            (0..(run.duration.as_millis()))
-                .flat_map(|hold| run.hold(Duration::from_millis(hold as u64)).then_some(hold))
+            (0..(run.duration))
+                .flat_map(|hold| run.hold(hold).then_some(hold))
                 .count()
         })
         .product();
@@ -50,6 +46,35 @@ fn a() {
     println!("{product}");
 }
 
+fn b() {
+    let duration: u64 = 42899189;
+    let record: u64 = 308117012911467;
+
+    if let (Some(first_win), Some(last_win)) = join(
+        || {
+            (0..=duration).find(|hold| {
+                let time_left = duration - hold;
+                let velocity = hold;
+                let distance = velocity * time_left;
+                distance > record
+            })
+        },
+        || {
+            (0..=duration).rev().find(|hold| {
+                let time_left = duration - hold;
+                let velocity = hold;
+                let distance = velocity * time_left;
+                distance > record
+            })
+        },
+    ) {
+        let wins = last_win - first_win + 1; // off by one
+
+        println!("{wins}");
+    }
+}
+
 fn main() {
     a();
+    b();
 }
